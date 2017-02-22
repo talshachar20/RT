59112eb2a14e44e6c2b3396d6ea40f437b143ef3
Tal Shachar <tal.shachar@auctionata.com>
2017-02-22 22:10:02 +0100

    debug

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by tal.shachar on 22/02/17.
+ */
+public class LoginPage extends DriverInit {
+
+    @FindBy(id="username")
+    private WebElement userName;
+
+    @FindBy(id="login_credentials_password")
+    private WebElement password;
+
+    @FindBy(id="login_submit")
+    private WebElement loginSubmit;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String usernameInput, String passwordInput) {
+
+        this.userName.clear();
+        this.userName.sendKeys(usernameInput);
+
+        this.password.clear();
+        this.password.sendKeys(passwordInput);
+
+        this.loginSubmit.click();
+    }
+}
